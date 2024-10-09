0fd653f758416b4f45aaa5fa34b7001363c1c914
bustyAI <oscar120646@gmail.com>
2024-10-08 21:48:36 -0700

    creating navbar

---
+++ src/components/SubHeader.jsx
@@ -0,0 +1,9 @@
+import styles from './SubHeader.module.css'
+
+export default function SubHeader() {
+  return (
+    <div className={styles.subHeader}>
+      <p className={styles.welcome}>Welcome</p>
+    </div>
+  )
+}
