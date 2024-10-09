8bc19618d595efb351262844dcec23e21613f6bc
Oscar Ochoa <eliseo.ochoa9925@yahoo.com>
2024-10-08 21:48:36 -0700

    creating navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+
+import SubHeader from "./SubHeader";
+import styles from './Header.module.css'
+
+// Icons
+import { RxHamburgerMenu } from 'react-icons/rx'
+import { GiAbstract005 } from 'react-icons/gi'
+
+export default function Header() {
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.upperNav}>
+        <GiAbstract005 className={styles.logo} />
+        <RxHamburgerMenu className={styles.hamburger} />
+      </div>
+      <div>
+        <SubHeader />
+      </div>
+    </nav>
+  )
+}
