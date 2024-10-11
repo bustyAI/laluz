f38f99fd0f8b7a134c541032a56ada6c90f101b9
Oscar Ochoa <eliseo.ochoa9925@yahoo.com>
2024-10-11 00:44:08 -0700

    adding About.jsx component + styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,44 @@
 
-import styles from './About.css/modules'
+import styles from './About.module.css'
+
+const aboutUs = {
+  "01": "Custom tattoo designs",
+  "02": "Expert artists team",
+  "03": "Safe, sterile environment",
+  "04": "Located downtown center",
+
+}
 
 export default function About() {
   return (
-    <div className={styles.aboutContainer}>About</div>
+    <section className={styles.about}>
+      <div className={styles.aboutContainer}>
+
+        <div className={styles.aboutHeadings}>
+          <div>
+            <h3>We Help</h3>
+          </div>
+          <div>
+            <h3>Tattoo People</h3>
+          </div>
+        </div>
+
+        <div className={styles.aboutUs}>
+
+          {Object.entries(aboutUs).map(([key, value]) => (
+            <div className={styles.aboutUsContent} key={key}>
+              <div>
+                <h3 className={styles.aboutUsNumber}>{key}</h3>
+              </div>
+              <div style={{ width: '100%' }}>
+                <h3 className={styles.aboutUsText}>{value}</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+
+      </div>
+
+    </section>
   )
 }
