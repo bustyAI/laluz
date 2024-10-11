23b0884e1b31879465684fd45f1e88b9af51de4e
bustyAI <oscar120646@gmail.com>
2024-10-11 00:44:08 -0700

    adding About.jsx component + styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Header from './components/Header'
 import Video from './components/Video'
+import About from './components/About'
 
 import './App.css'
+
 
 function App() {
   return (
     <div>
       <Header />
       <Video />
+      <About />
     </div>
 
   )
