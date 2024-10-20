35a9eec24a803c35da0ba28806fea8306791f360
bustyAI <oscar120646@gmail.com>
2024-10-20 09:05:37 -0700

    refactoring containers + adding fonts

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -3,15 +3,17 @@ import styles from './ArtistCard.module.css'
 export default function ArtistCard() {
   return (
 
-    <div className={styles.artistCard}>
+    <a className={styles.artistCard}>
+      <div className={styles.artistCardContent}>
 
-      <img className={styles.artistImage} src='./tattoo-artist.jpg' alt='tattoo-artist' />
-      <h4 className={styles.artistName}>Hannah Pesina</h4>
+        <img className={styles.artistImage} src='./tattoo-artist.jpg' alt='tattoo-artist' />
 
-      <h4 className={styles.jobTitle}>Positivity Princess</h4>
-      <a href='#'>
         <h4 className={styles.socialHandle}>@xhv.zyy</h4>
-      </a>
-    </div>
+
+        <h4 className={styles.jobTitle}>Positivity Princess</h4>
+        <h4 className={styles.artistExperience}>20 years in positivity / making people feel good about themselves</h4>
+
+      </div>
+    </a>
   )
 }
