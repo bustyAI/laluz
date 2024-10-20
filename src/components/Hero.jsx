9bb0b66a851f065d793a831ecd60599328461a55
Oscar Ochoa <eliseo.ochoa9925@yahoo.com>
2024-10-20 09:05:37 -0700

    refactoring containers + adding fonts

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,23 @@
 import ArtistCard from './ArtistCard'
 
-import { GoArrowDown } from 'react-icons/go'
-
 import styles from './Hero.module.css'
 
 export default function Hero() {
   return (
-    <div className={styles.heroContainer}>
+    <>
       <div className={styles.meetOurArtist}>
-        <h3>meet our artist:</h3>
-        <h3><GoArrowDown /></h3>
+        <h1>meet our artist</h1>
       </div>
-      <div className={styles.artistCardContainer}>
-        <ArtistCard />
-        <ArtistCard />
-        <ArtistCard />
-        <ArtistCard />
-        <ArtistCard />
-        <ArtistCard />
+
+      <div className={styles.heroContainer}>
+
+        <div className={styles.artistCardContainer}>
+          <ArtistCard />
+          <ArtistCard />
+          <ArtistCard />
+          <ArtistCard />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
