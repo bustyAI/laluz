c2f2c79745c3a4538994178b3e81bb6afe2d9e73
bustyAI <oscar120646@gmail.com>
2024-10-20 22:14:18 -0700

    creating carousel for images

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,19 +1,46 @@
+import { useState } from 'react'
+
+import { BsArrowLeftCircle } from "react-icons/bs";
+import { BsArrowRightCircle } from "react-icons/bs";
 import styles from './ArtistCard.module.css'
 
+const tattooImages = [
+  'public/tattoo-photo.jpg',
+  'public/tattoo-photo-2.jpg',
+
+]
+
 export default function ArtistCard() {
+  const [currImageIndex, setCurrImageIndex] = useState(0);
+
+  const nextImage = () => {
+    setCurrImageIndex((prevIndex) => (prevIndex + 1) % tattooImages.length)
+  }
+
+  const prevImage = () => {
+    setCurrImageIndex((prevIndex) => (prevIndex - 1 + tattooImages.length) % tattooImages.length)
+  }
+
   return (
 
-    <a className={styles.artistCard}>
+    <div className={styles.artistCard}>
       <div className={styles.artistCardContent}>
+        <div className={styles.imageCarousel}>
+          <BsArrowLeftCircle className={styles.arrowIcon} onClick={prevImage} />
+          <img
+            className={styles.artistImage}
+            src={tattooImages[currImageIndex]}
+            alt='tattoo-artist' />
+          <BsArrowRightCircle className={styles.arrowIcon} onClick={nextImage} />
 
-        <img className={styles.artistImage} src='./tattoo-artist.jpg' alt='tattoo-artist' />
+        </div>
 
-        <h4 className={styles.socialHandle}>@xhv.zyy</h4>
+        <a href='#' className={styles.socialHandle}>@xhv.zyy</a>
 
         <h4 className={styles.jobTitle}>Positivity Princess</h4>
         <h4 className={styles.artistExperience}>20 years in positivity / making people feel good about themselves</h4>
 
       </div>
-    </a>
+    </div>
   )
 }
