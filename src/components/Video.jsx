bcf4c69b3f9f96584f26435c2d978e731e72c213
bustyAI <oscar120646@gmail.com>
2024-10-23 05:34:40 -0700

    changing to fig

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,10 +3,7 @@ import styles from './Video.module.css'
 export default function Video() {
   return (
     <div className={styles.videoContainer}>
-      <video autoPlay muted loop>
-        <source src='/tattoo-vid.mp4' type='video/mp4' />
-        Browser Does not support the video tag.
-      </video>
+      <img src='/tattoo-vid.gif' alt='tattoo-video' />
       <div className={styles.videoTextContainer}>
         <div className={styles.videoText}>La Luz Tattoo</div>
       </div>
