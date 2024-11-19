5303499f84e5b46ffbbfd4847232d81610af6290
bustyAI <eliseo.ochoa9925@yahoo.com>
2024-11-19 10:22:30 -0800

    adding scrool effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,17 @@
 import ArtistCard from './ArtistCard'
 
+import { AnimationOnScroll } from 'react-animation-on-scroll'
+
 import styles from './Hero.module.css'
 
 export default function Hero() {
   return (
     <>
-      <div className={styles.meetOurArtist}>
-        <h1>meet our artist</h1>
-      </div>
+      <AnimationOnScroll animateOnce={true} animateIn={styles.fadeInUp}>
+        <div className={styles.meetOurArtist}>
+          <h1>meet our artist</h1>
+        </div>
+      </AnimationOnScroll>
 
       <div className={styles.heroContainer}>
         <div className={styles.artistCardContainer}>
